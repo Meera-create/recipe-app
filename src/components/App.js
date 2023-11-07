408bd9301682b590c83bda7a081c7583313e1fb7
dylstar135 <dylstar160990@googlemail.com>
2023-11-07 20:52:58 +0000

    first attemp at recipe finder form (no api call yet)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
-import '../styles/App.css';
+import React, { useState } from 'react';
+import RecipeFinderForm from '/home/dylstar/Projects/recipe-app/src/components/recipeFinder.js/RecipeFinderForm.js';
 
 function App() {
+  const [ingredientList, setIngredientList] = useState([]);
+  const [recipes, setRecipes] = useState([]);
+
+  const searchRecipes = async (ingredients) => {
+   
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Recipe App</h1>
-      </header>
+    <div>
+      <h1>Recipe Finder</h1>
+      <RecipeFinderForm onSearch={searchRecipes} />
+      <ul>
+        {recipes.map((recipe, index) => (
+          <li key={index}>{recipe.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
