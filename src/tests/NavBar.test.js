4f02d8cb52ace0b77b3b751f6371a0d35e6e87bc
dylstar135 <dylstar160990@googlemail.com>
2023-11-09 20:04:55 +0000

    nav test, and app test pass

--- src/tests/NavBar.test.js
+++ src/tests/NavBar.test.js
@@ -2,15 +2,18 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import { AuthContext } from "../Context/AuthContext";  // Import the real AuthContext
 
 describe("NavBar", () => {
   it("renders correctly", () => {
     const { asFragment } = render(
       <MemoryRouter>
-        <NavBar />
+        <AuthContext> {/* Use the real AuthContext here */}
+          <NavBar />
+        </AuthContext>
       </MemoryRouter>
     );
 
     expect(asFragment()).toMatchSnapshot();
-  })
+  });
 });
