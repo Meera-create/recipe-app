f832f44e6c774cc4d4f68d385355ac37764a710a
Tim <50870607+toothless13@users.noreply.github.com>
2023-11-09 21:39:51 +0000

    Include add button to add ingredients to array

--- src/components/RecipeFinder/RecipeHomePage.js
+++ src/components/RecipeFinder/RecipeHomePage.js
@@ -5,10 +5,17 @@ import RecipeAll from './RecipesAll';
 const RecipeHomePage = () => {
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState(false);
+  const [ingredientsList, setIngredientsList] = useState([]);
 
   return (
     <div className='recipe-homepage'>
-      <RecipeFinderForm recipes={recipes} setRecipes={setRecipes} setSearch={setSearch}/>
+      <RecipeFinderForm 
+        recipes={recipes} 
+        setRecipes={setRecipes} 
+        setSearch={setSearch} 
+        ingredientsList={ingredientsList} 
+        setIngredientsList={setIngredientsList}
+      />
       <RecipeAll recipes={recipes} search={search}/>
     </div>
   )
