63ccbe6fd6075623098b186b9fba11d210f8f15b
Tim <50870607+toothless13@users.noreply.github.com>
2023-11-17 12:57:29 +0000

    start basic add-recipe form

--- src/components/RecipeFinder/AddRecipeForm.js
+++ src/components/RecipeFinder/AddRecipeForm.js
@@ -1,9 +1,52 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { Context } from '../../Context/AuthContext';
 
 
 const AddRecipeForm = () => {
+  const { user } = useContext(Context);
+
+  const initialState = {
+    fields: {
+      title: '',
+      extendedIngredients: [],
+      instructions: '',
+      readyInMinutes: ''
+    }
+  }
+
+  const [fields, setFields] = useState(initialState.fields);
+
+  const handleFieldChange = (event) => {
+    const { name, value } = event.target;
+    setFields({ ...fields, [name]: value });
+  }
+
   return (
-    <div>AddRecipeForm</div>
+    <div>
+      <h1>Add a recipe</h1>
+      <form>
+        <label htmlFor='title'>Recipe Name: </label>
+        <input 
+          id='title' 
+          name='title' 
+          value={fields.title} 
+          onChange={handleFieldChange}
+        />
+        <br />
+        <br />
+        <label htmlFor='readyInMinutes'>Time to cook (minutes): </label>
+        <input 
+          type="number" 
+          id="readyInMinutes" 
+          name="readyInMinutes" 
+          value={fields.readyInMinutes} 
+          onChange={handleFieldChange} 
+        />
+        <br />
+        <br />
+        <button type="button">Add Recipe!</button>
+      </form>
+    </div>
   )
 }
 
