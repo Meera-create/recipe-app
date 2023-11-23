fa9b40f2699f352ecbb615b9c419e6524b734a54
dylstar135 <dylstar160990@googlemail.com>
2023-11-23 21:43:39 +0000

    thumbanil on saved

--- src/components/RecipeFinder/SavedRecipe.js
+++ src/components/RecipeFinder/SavedRecipe.js
@@ -2,31 +2,46 @@ import React from 'react';
 import parse from 'html-react-parser';
 
 const SavedRecipe = ({ recipe }) => {
+  const buttonStyle = {
+    display: 'none',
+    visibility: 'hidden',
+    opacity: 0,
+    position: 'relative', // Change to 'relative' or remove this line
+  };
+  
 
   return (
     <div className="single_recipe">
-        <div className="clicked-recipe">
-            <h2>{recipe.title}</h2>
-            <div className="full-recipe">
-          {recipe.image !== undefined && <div>
-              <img className="image" alt="pic of food"  src={recipe.image} />
-          </div>}          
-    <div >
-      <h3>Ingredients:</h3>
-      <ul>
-          {recipe.extendedIngredients.map((ingredient, index) => (
-            <li key={index}>{ingredient.original}</li>
-          ))}
-      </ul>
-            </div>
-            <h3>Instructions:</h3>
-                {parse(`${recipe.instructions}`)}   
-            <h3>Time to cook: {recipe.readyInMinutes} minutes </h3>
-            
-            </div>
+      <div className="clicked-recipe">
+        <button style={buttonStyle}>Hidden Button</button>
+
+        {recipe.image !== undefined && (
+          <div>
+            <img className="image" alt="pic of food" src={recipe.image} />
+          </div>
+        )}
+
+        <h2>{recipe.title}</h2>
+
+        <div>
+          <h3>Ingredients:</h3>
+          <ul>
+            {recipe.extendedIngredients.map((ingredient, index) => (
+              <li key={index}>{ingredient.original}</li>
+            ))}
+          </ul>
         </div>
-  </div>
-  )
-}
+
+        <h3>Instructions:</h3>
+        {parse(`${recipe.instructions}`)}
+
+        <h3>Time to cook: {recipe.readyInMinutes} minutes </h3>
+      </div>
+    </div>
+  );
+};
 
 export default SavedRecipe;
+
+
+
