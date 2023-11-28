4bd416a24063e5125e23118138c95b34abc0025a
Tim <50870607+toothless13@users.noreply.github.com>
2023-11-28 19:17:55 +0000

    change favicon and center text on landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -13,8 +13,7 @@ const Landing = () => {
             <h2 className="sub">Not a problem!</h2>
             <button className="button-enter"type="button">
                 <NavLink to="homepage">ENTER</NavLink>
-                </button>
-             
+            </button>  
         </div>
     )
 }
