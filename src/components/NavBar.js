268eb3fe6ec542539b0ed04df4290c15581685a5
Tim <50870607+toothless13@users.noreply.github.com>
2023-11-28 18:04:42 +0000

    add responsive navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,22 @@ import logo from '../../src/styles/images/logo.jpg'
 const NavBar = () => {
   const { user } = useContext(Context);
 
+  const handleHamburgerClick = () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".boxes2");
+
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }
+
+  const handleCloseHamburger = () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".boxes2");
+
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
+
   if (user) {
     return (
       <div className="navbar">
@@ -16,23 +32,25 @@ const NavBar = () => {
               <Link reloadDocument to="/recipe-form">
                 <img className="logo" alt="logo" src={logo} />
               </Link>
-          </div>
-          
+            </div>
             <div className="boxes2">
-              <button type="button" className="add-recipe">
+              <button type="button" className="nav-item add-recipe" onClick={handleCloseHamburger}>
                 <NavLink to="add-recipe">Add Recipe</NavLink>
-            </button>
-
-             
-              <button type="button" className="recipe-form-button">
+              </button>
+              <button type="button" className="nav-item recipe-form-button" onClick={handleCloseHamburger}>
                 <NavLink to="recipe-form">Recipe Finder</NavLink>
               </button>
-             
-           
-              <button type="button"className="my-account">
+              <button type="button" className="nav-item my-account" onClick={handleCloseHamburger}>
                 <NavLink to="my-account">My Account</NavLink>
               </button>
-              <Logout />
+              <div className="nav-item logout" onClick={handleCloseHamburger} >
+                <Logout />
+              </div>
+            </div>
+            <div className="hamburger" onClick={handleHamburgerClick}>
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span className="bar"></span>
             </div>
           </div>
       </div>
@@ -45,17 +63,19 @@ const NavBar = () => {
               <Link reloadDocument to="/recipe-form">
                 <img className="logo" alt="logo" src={logo} />
               </Link>
-          </div>
-         
-
-          <div className="boxes2">
-            
-              <button type="button" className="login">
+            </div>
+            <div className="boxes2">
+              <button type="button" className="nav-item login" onClick={handleCloseHamburger}>
                 <NavLink to="login">Login</NavLink>
               </button>
-              <button type="button"className="sign-up">
+              <button type="button"className="nav-item sign-up" onClick={handleCloseHamburger}>
                 <NavLink to="sign-up">Sign Up</NavLink>
               </button>
+            </div>
+            <div className="hamburger" onClick={handleHamburgerClick}>
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span className="bar"></span>
             </div>
           </div>
       </div>
